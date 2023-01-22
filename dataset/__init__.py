cbfe4cb18732729228c7b1b5925ef165041351a6
anonymous-synonymous <qj19962@bristol.ac.uk>
2023-01-22 09:57:52 +0000

    Anonymising google drive link

--- dataset/__init__.py
+++
@@ -1,1 +0,0 @@
-from dataset.gran_data import *
--- download.sh
+++ download.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-mkdir -p exp
-cd exp
-
-echo "Downloading exp.zip from https://drive.google.com/file/d/1vz8n1daLZj8nKhsESbh-wGAa-ZlqUhu0/view?usp=sharing"
+echo "Downloading .zip"
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1jafpd2Hl9qyjUCYtehVZGxQI-4GdX6nw" -O exp.zip && rm -rf /tmp/cookies.txt
+echo "Unpacking zip"
 unzip exp.zip
+echo "Removing .zip"
 rm exp.zip
 
 
